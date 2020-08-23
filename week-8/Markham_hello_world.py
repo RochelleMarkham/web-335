28a6d554216179af9dae1bc033c5404381543fc7
Rochelle Markham <rmarkham@my365.bellevue.edu>
2020-08-23 12:54:08 -0500

    Exercise 8.2

---
+++ week-8/Markham_hello_world.py
@@ -0,0 +1,11 @@
+#
+#================================
+# Title: Hello World
+# Author: Professor Krasso
+# Date: 8/23/20
+# Modified By: Rochelle Markham
+# Description: Basic Hello World
+#================================
+#
+
+print('Hello World')
