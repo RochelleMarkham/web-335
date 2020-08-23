4d0f873e195c7ea0850ff8243e523b3d0a3ad3db
Rochelle Markham <rmarkham@my365.bellevue.edu>
2020-08-23 13:51:28 -0500

    Exercise 8.3

---
+++ week-8/Markham_calculator.py
@@ -0,0 +1,27 @@
+#
+#================================
+# Title: Calculator
+# Author: Professor Krasso
+# Date: 8/23/20
+# Modified By: Rochelle Markham
+# Description: Basic Calculator
+#================================
+#
+
+#adds and returns sum of 2 numbers
+def add(num1, num2):
+    return(num1 + num2)
+
+print(add(1, 2))
+
+#subtracts one number from another and returns the value
+def subtract(num3, num4):
+    return(num3-num4)
+
+print(subtract(4,1))
+
+#divides one number by another and returns the value
+def divide(num5, num6):
+    return(num5/num6)
+
+print(divide(8,2))
